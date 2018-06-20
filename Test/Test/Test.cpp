a03bef472ebb2e8c7e5311877c0aa062f8481d91
javametro <minda1949@sina.com>
2018-06-20 13:43:08 +0800

    create project

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -2,13 +2,30 @@
 //
 
 #include "stdafx.h"
+#include "Cardgame.h"
 #include <iostream>
 using namespace std;
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "hello world" << endl;
+	Cardgame *bridge = 0;
+	Cardgame *blackjack = 0;
+	Cardgame *solitaire = 0;
+	Cardgame *poker = 0;
+
+	bridge = new Cardgame(4);
+	blackjack = new Cardgame(8);
+	solitaire = new Cardgame(1);
+
+	delete blackjack;
+	delete bridge;
+	
+	poker = new Cardgame(5);
+	delete solitaire;
+	delete poker;
+
+	cin.get();
 	return 0;
 }
 
