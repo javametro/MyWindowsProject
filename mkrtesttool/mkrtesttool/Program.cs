047a7bf4d634c778b7b080195653b57cf46ba5ab
javametro <minda1949@sina.com>
2018-07-02 17:14:09 +0800

    add checknecsign and check drive c & d function

--- mkrtesttool/mkrtesttool/Program.cs
+++ mkrtesttool/mkrtesttool/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Win32;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace mkrtesttool
 {
@@ -11,12 +13,20 @@ namespace mkrtesttool
     {
         static void Main(string[] args)
         {
+            CheckNECModel();
+            CheckNECSign();
+            CheckFolder();
+
+        }
+
+        public static void CheckNECModel()
+        {
             string value = "";
             RegistryKey hklm = Registry.LocalMachine;
             RegistryKey store_content_modifier = hklm.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Store\\");
             value = (string)store_content_modifier.GetValue("StoreContentModifier");
 
-            if(value.CompareTo("Lenovo5_NECPC1") == 0)
+            if (value.CompareTo("Lenovo5_NECPC1") == 0)
             {
                 Console.WriteLine("Consumer Model");
             }
@@ -28,6 +38,77 @@ namespace mkrtesttool
             {
                 Console.WriteLine("Not NEC Model");
             }
+
+        }
+
+        public static void CheckNECSign()
+        {
+            string patten = @"B3CD3740.V2_\d.\d.\d.\d_x64__md25j3s46526j";
+            Regex rgx = new Regex(patten, RegexOptions.IgnoreCase);
+            string folder = "";
+            string windows_apps = @"C:\Program Files\WindowsApps";
+            DirectoryInfo dirs = new DirectoryInfo(windows_apps);
+            if (dirs.Exists)
+            {
+                foreach(DirectoryInfo diritem in dirs.GetDirectories())
+                {
+                    MatchCollection matches = rgx.Matches(diritem.FullName);
+                    if(matches.Count > 0)
+                    {
+                        foreach(Match match in matches)
+                        {
+                            folder = windows_apps + "\\" + match.Value;
+                        }
+                    }
+                    
+                }
+                
+            }
+
+            Console.WriteLine(folder);
+
+            string sigcheck_tool = @"C:\software\tools\sigcheck.exe";
+            string cmdStr = " -e " + "\"" + folder + "\"";
+            System.Diagnostics.Process exep = new System.Diagnostics.Process();
+            exep.StartInfo.FileName = sigcheck_tool;
+            exep.StartInfo.Arguments = cmdStr;
+            exep.StartInfo.CreateNoWindow = false;
+            exep.StartInfo.UseShellExecute = false;
+            exep.Start();
+            exep.WaitForExit();
+
+
+        }
+
+
+        public static void CheckFolder()
+        {
+            string[] drives = Environment.GetLogicalDrives();
+            //    Console.WriteLine("GetLogicalDrives: {0}", String.Join(", ", drives));
+
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach(DriveInfo drive in allDrives)
+            {
+                //Console.WriteLine(drive.Name + "----" + drive.DriveType);
+                if (!drive.DriveType.Equals(DriveType.CDRom))
+                {
+                    if (drive.Name.Equals("C:\\") || drive.Name.Equals("D:\\"))
+                    {
+                        DirectoryInfo directoryInfo = new DirectoryInfo(drive.Name);
+                        // Console.WriteLine(drive);
+                        DirectoryInfo[] dirs = directoryInfo.GetDirectories();
+                        foreach (DirectoryInfo dir in dirs)
+                        {
+                            Console.WriteLine(dir.FullName);
+                        }
+                    }
+                }
+
+                
+            }
         }
     }
+
+    
 }
