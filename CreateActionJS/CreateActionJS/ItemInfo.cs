8aac998d57b1d2005541589450063b5842dc873e
javametro <minda1949@sina.com>
2018-12-11 16:23:34 +0800

    add new file: CreateActionJS/CreateActionJS/ItemInfo.cs

---
+++ CreateActionJS/CreateActionJS/ItemInfo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CreateActionJS
+{
+    class ItemInfo : INotifyPropertyChanged
+    {
+        private string itemName;
+        private string automationId;
+        private string helpText;
+
+        public ItemInfo(string name, string id, string text)
+        {
+            this.itemName = name;
+            this.automationId = id;
+            this.helpText = text;
+        }
+
+        public ItemInfo()
+        {
+
+        }
+
+        public string ItemName
+        {
+            get
+            {
+                return itemName;
+            }
+
+            set
+            {
+                if (itemName != value)
+                {
+                    itemName = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("ItemName"));
+                }
+            }
+        }
+
+        public string AutomationId
+        {
+            get
+            {
+                return automationId;
+            }
+
+            set
+            {
+                if (automationId != value)
+                {
+                    automationId = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("AutomationId"));
+                }
+            }
+        }
+
+        public string HelpText
+        {
+            get
+            {
+                return helpText;
+            }
+
+            set
+            {
+                if (helpText != value)
+                {
+                    helpText = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("HelpText"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
