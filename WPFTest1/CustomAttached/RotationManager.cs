cbf486e0934e899e0ffce2cc7c780a0b9469809e
javametro <minda1949@sina.com>
2018-12-11 16:22:03 +0800

    add project WPFTEst1

---
+++ WPFTest1/CustomAttached/RotationManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAttached
+{
+    class RotationManager : DependencyObject
+    {
+
+
+        public static int GetMyProperty(DependencyObject obj)
+        {
+            return (int)obj.GetValue(MyPropertyProperty);
+        }
+
+        public static void SetMyProperty(DependencyObject obj, int value)
+        {
+            obj.SetValue(MyPropertyProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.RegisterAttached("MyProperty", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
+
+
+    }
+}
---
+++ WPFTest1/DependencyProperties/SimpleControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DependencyProperties
+{
+    /// <summary>
+    /// Interaction logic for SimpleControl.xaml
+    /// </summary>
+    public partial class SimpleControl : UserControl
+    {
+        public SimpleControl()
+        {
+            InitializeComponent();
+        }
+
+        //public int MyProperty {
+        //       get { return (int)GetValue(MyProperty); }
+        //       set { SetValue(MyProperty, value); }
+        //}
+
+        //public static readonly DependencyProperty MyPropertyProperty = DependencyProperty.Register("MyProperty", typeof(int), typeof(SimpleControl), new UIPropertyMetadata(0));
+
+        public int YearPublished {
+               get { return (int)GetValue(YearPublishedProperty); }
+                set { SetValue(YearPublishedProperty, value); }
+        }
+
+        public static readonly DependencyProperty YearPublishedProperty = DependencyProperty.Register("YearPublished", typeof(int), typeof(SimpleControl), new UIPropertyMetadata(2000));
+    }
+}
