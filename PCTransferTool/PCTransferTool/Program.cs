aed5bcd008c78504d0d646a614d12912645649a2
javametro <minda1949@sina.com>
2018-12-11 16:19:47 +0800

    add project PCTransferTool

---
+++ PCTransferTool/PCTransferTool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCTransferTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to use PCTransfer Tool.");
+            string document_folder = @"C:\users\stephen\documents";
+            string download_folder = @"C:\users\stephen\downloads";
+            string desktop_folder = @"C:\users\stephen\desktop";
+            
+        }
+    }
+}
